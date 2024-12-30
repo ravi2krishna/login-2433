4d41049038f3938b68a832238e46ceefc7de8d80
ravi2krishna <ravi2krishna@gmail.com>
2024-12-30 02:11:01 +0000

    create vnet

---
+++ tf-az-demo/az.tf
@@ -0,0 +1,18 @@
+# Create a resource group
+resource "azurerm_resource_group" "lms-2433" {
+  name     = "2433-rg"
+  location = "East Us"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "lms" {
+  name                = "lms-vnet"
+  resource_group_name = azurerm_resource_group.lms-2433.name
+  location            = azurerm_resource_group.lms-2433.location
+  address_space       = ["10.0.0.0/16"]
+  tags = {
+    environment = "dev"
+  }
+}
+
+
