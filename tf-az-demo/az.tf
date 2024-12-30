3ab3a5dc265a007ba7f8fec79080c2f2ed86174e
ravi2krishna <ravi2krishna@gmail.com>
2024-12-30 07:59:57 +0530

    create nic web

--- tf-az-demo/az.tf
+++ tf-az-demo/az.tf
@@ -50,3 +50,17 @@ resource "azurerm_public_ip" "lms-pip" {
     environment = "lms-web-pip"
   }
 }
+
+# NIC
+resource "azurerm_network_interface" "lms-web-nic" {
+  name                = "lms-web-nic"
+  location            = azurerm_resource_group.lms-2433.location
+  resource_group_name = azurerm_resource_group.lms-2433.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.lms-web-sn.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.lms-pip.id
+  }
+}
